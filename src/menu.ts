e209374e3109a9a2a0ac7f7a776b8b811e02ba90
makometr <makometr@yandex.ru>
2018-11-25 21:25:41 +0300

    Some styles to menu page added

--- src/menu.ts
+++ src/menu.ts
@@ -1,3 +1,6 @@
 function startGame(){
     console.log("game started!");
+    let inputElem: HTMLInputElement = document.getElementById("inputName") as HTMLInputElement;
+    let name: string = inputElem.value;
+    console.log(name);
 }
